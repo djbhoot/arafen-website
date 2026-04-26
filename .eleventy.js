df29936d7f8355aa8347f467ffbd2003b4df27f7
Arafen K. Shovon <98313502+djbhoot@users.noreply.github.com>
2026-04-27 02:32:51 +0600

    Ignore duplicate legacy article folder pages

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,10 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.ignores.add("node_modules/**");
   eleventyConfig.ignores.add("MIGRATION_NOTES.md");
   eleventyConfig.ignores.add("src/index.njk");
+  eleventyConfig.ignores.add("blog/b2b-gtm-strategy-framework/index.html");
+  eleventyConfig.ignores.add("blog/b2b-saas-seo-content-strategy/index.html");
+  eleventyConfig.ignores.add("blog/best-cold-email-sequence/index.html");
+  eleventyConfig.ignores.add("blog/top-5-gtm-stack/index.html");
 
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("IMG_1256.jpg");
