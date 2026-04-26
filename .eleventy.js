3a5c171084a4408f7ef94e2edefda04454a464d1
Arafen K. Shovon <98313502+djbhoot@users.noreply.github.com>
2026-04-27 01:28:57 +0600

    Build from root to preserve existing site design

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,22 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.ignores.add("_site/**");
+  eleventyConfig.ignores.add("node_modules/**");
+  eleventyConfig.ignores.add("MIGRATION_NOTES.md");
+
   eleventyConfig.addPassthroughCopy("admin");
-  eleventyConfig.addPassthroughCopy("tools");
+  eleventyConfig.addPassthroughCopy("IMG_1256.jpg");
+  eleventyConfig.addPassthroughCopy("favicon.png");
+  eleventyConfig.addPassthroughCopy("_redirects");
+  eleventyConfig.addPassthroughCopy("robots.txt");
+  eleventyConfig.addPassthroughCopy("sitemap.xml");
 
   return {
     dir: {
-      input: "src",
-      includes: "_includes",
+      input: ".",
+      includes: "src/_includes",
       output: "_site"
-    }
+    },
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk"
   };
 };
